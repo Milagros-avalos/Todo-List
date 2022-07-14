8e6f79f7fe1e2a8e8d54e67b7265e2a255fb24ca
Milagros Avalos <93937678+Milagros-avalos@users.noreply.github.com>
2022-07-14 16:24:37 -0300

    Cambio en la direccion package.json

---
+++ src/App/AppUI.js
@@ -0,0 +1,55 @@
+import React from 'react';
+// También es importante importar nuestro contexto
+import { TodoContext } from '../TodoContext';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { TodoForm } from "../TodoForm";
+import { CreateTodoButton } from '../CreateTodoButton';
+import { Modal } from '../Modal';
+
+function AppUI() {
+  // Desesctructuramos los valores de nuestro contexto
+  const {
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+  } = React.useContext(TodoContext);
+  
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        {error && <p> Error de la Pagina...</p>}
+        {loading && <p>Cargando...</p>}
+        {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
+        
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+
+      {!!openModal && (
+				<Modal>
+					<TodoForm />
+				</Modal>
+			)}
+			<CreateTodoButton setOpenModal={setOpenModal} />
+		</React.Fragment>
+  );
+}
+
+export { AppUI };
